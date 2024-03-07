dc17bb3f252c17163a1e39b0ec3e657bb9be53a7
Manuel Ruiz U <mruiz.u@icloud.com>
2024-03-07 10:03:27 -0300

    agregando nuevas funciones con jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,18 @@ $(document).ready(function(){
     $("h1.color1").click(function(){
         $(this).css("color", "red");
     });
+
+    $("p.color2").click(function(){
+        $(this).css("color", "green");
+    });
+
+    $("h1.color1").dblclick(function(){
+        $(this).css("color", "black");
+    });
+
+    $("p.color2").dblclick(function(){
+        $(this).css("color", "black");
+    });
 });
 
 
